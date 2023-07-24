8f24fa4c5b21d85bc9ba7ef2637bed6697bca9d0
Juan Martín García <hello@juangarcia.com.ar>
2023-07-24 10:38:39 -0300

    Basic implementation for full screen navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,32 @@
+"use client";
+
+import ReactFullpage from '@fullpage/react-fullpage';
+
 import Header from "@/components/Header";
 
 export default function Home() {
   return (
     <main>
       <Header />
+      <ReactFullpage
+        credits={{ enabled: false }}
+        licenseKey=''
+        // navigation
+        // pluginWrapper={pluginWrapper}
+        // onLeave={onLeave}
+        // scrollHorizontally={true}
+        // sectionsColor={sectionsColor}
+        render={() => (
+          <ReactFullpage.Wrapper>
+            <section className='section'>
+              test 1
+            </section>
+            <section className='section'>
+              test 2
+            </section>
+          </ReactFullpage.Wrapper>
+        )}
+      />
     </main>
   )
 }
