347f0bf38ee32d275e68d7de582b8a26fce84f48
Juan Martín García <hello@juangarcia.com.ar>
2023-07-25 10:40:38 -0300

    Add basic interactive cursor functionality

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,27 @@
 import '@/styles/globals.css';
+
 import type { AppProps } from 'next/app';
+import dynamic from 'next/dynamic';
+
+const AnimatedCursor = dynamic(() => import('react-animated-cursor'), {
+  ssr: false
+});
  
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <AnimatedCursor
+        innerSize={0}
+        outerSize={35}
+        innerScale={1}
+        outerScale={2}
+        outerAlpha={0}
+        outerStyle={{
+          border: '2px solid var(--cursor-color)',
+          mixBlendMode: 'darken'
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  )
 }
