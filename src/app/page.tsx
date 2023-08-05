4ff2b5b6915d5207cda28f847d6b605fc5f193f8
Juan Martín García <hello@juangarcia.com.ar>
2023-08-05 15:23:43 -0300

    Basic under construction page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,37 +11,23 @@ import { MainHeading } from "@/components/Typography";
 import { attributes } from '@/content/homepage.md';
 
 export default function Home() {
-  const { slogan, slider, architectsAndDevelopersHover, brokersAndRealtorsHover } = attributes;
+  const { slogan, slider } = attributes;
 
   return (
     <main>
-      <CustomHead
-        // title={title}
-      />
-      <Header />
-      <ReactFullpage
-        credits={{ enabled: false }}
-        licenseKey={process.env.NEXT_PUBLIC_FULLPAGE_LICENSE || ''}
-        render={() => (
-          <ReactFullpage.Wrapper>
-            <section className="section">
-              <h1 className="absolute md:left-16 z-10 w-full h-full flex items-center drop-shadow-lg">
-                <MainHeading color="text-off-white" classes="max-w-[200px] md:max-w-lg mx-auto md:mx-0 text-center md:text-left">{slogan}</MainHeading>
-              </h1>
-              <FullPageSlider
-                slides={slider}
-                drag={true}
-              />
-            </section>
-            <section className="section">
-              <LandingPageSelector
-                architectsAndDevelopersHover={architectsAndDevelopersHover}
-                brokersAndRealtorsHover={brokersAndRealtorsHover}
-              />
-            </section>
-          </ReactFullpage.Wrapper>
-        )}
-      />
+      <a
+        href="https://instagram.com/"
+        target='_blank'
+        rel="noopener noreferrer"
+      >
+        <h1 className="absolute md:left-16 z-10 w-full h-full flex items-center drop-shadow-lg">
+          <MainHeading color="text-off-white" classes="max-w-[200px] md:max-w-3xl mx-auto md:mx-0 text-center md:text-left">{slogan}</MainHeading>
+        </h1>
+        <FullPageSlider
+          slides={slider}
+          drag={true}
+        />
+      </a>
     </main>
   )
 }
--- src/content/homepage.md
+++ src/content/homepage.md
@@ -1,5 +1,5 @@
 ---
-slogan: Visual solutions for Architecture & Real Estate
+slogan: "While me make changes to our site please find us on Instagram: @sharpstudiovisuals"
 slider:
   - image: /images/home/slide-1.jpg
     title: Competition for the New Administration Centre in Rawson
