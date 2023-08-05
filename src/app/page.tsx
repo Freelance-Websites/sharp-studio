5fb66037aed139ee8f3943e686f9ceb641021264
Juan Martín García <hello@juangarcia.com.ar>
2023-08-05 15:17:24 -0300

    Add back arrows for now

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 
 import ReactFullpage from '@fullpage/react-fullpage';
 
+import CustomHead from "@/components/CustomHead";
 import Header from "@/components/Header";
 import FullPageSlider from "@/components/sections/FullPageSlider";
 import LandingPageSelector from "@/components/LandingPageSelector";
@@ -14,6 +15,9 @@ export default function Home() {
 
   return (
     <main>
+      <CustomHead
+        // title={title}
+      />
       <Header />
       <ReactFullpage
         credits={{ enabled: false }}
@@ -21,11 +25,12 @@ export default function Home() {
         render={() => (
           <ReactFullpage.Wrapper>
             <section className="section">
-              <h1 className="absolute left-4 z-10 h-full flex items-center drop-shadow-lg max-w-xs md:max-w-lg">
-                <MainHeading color="text-off-white">{slogan}</MainHeading>
+              <h1 className="absolute md:left-16 z-10 w-full h-full flex items-center drop-shadow-lg">
+                <MainHeading color="text-off-white" classes="max-w-[200px] md:max-w-lg mx-auto md:mx-0 text-center md:text-left">{slogan}</MainHeading>
               </h1>
               <FullPageSlider
                 slides={slider}
+                drag={true}
               />
             </section>
             <section className="section">
