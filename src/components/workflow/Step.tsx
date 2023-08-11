3b9bd06fb9de374cd159f91a743ead16170efcac
Juan Martín García <hello@juangarcia.com.ar>
2023-08-11 09:32:57 -0300

    Workflow responsive adjustments

--- src/components/workflow/Step.tsx
+++ src/components/workflow/Step.tsx
@@ -9,7 +9,8 @@ export default function Step({
   imageCredit,
   title,
   description,
-  cta
+  ctaText,
+  ctaAction
  }: {
   index: number;
   image: string;
@@ -17,15 +18,16 @@ export default function Step({
   imageCredit: string;
   title: string;
   description: string;
-  cta: string;
+  ctaText: string;
+  ctaAction: () => void;
 }) {
   let formattedLinkText;
-  formattedLinkText = cta?.replace(/Contact us/gi, '<u class="decoration-1 underline-offset-4 md:underline-offset-8 hover:no-underline">$&</u>');
+  formattedLinkText = ctaText?.replace(/Contact us/gi, '<u class="decoration-1 underline-offset-4 md:underline-offset-8 hover:no-underline">$&</u>');
   formattedLinkText = formattedLinkText?.replace(/->/gi, `<i class="absolute top-1 md:top-3 md:ml-2 transform scale-75 md:scale-100"><svg class="fill-black" fill="none" height="21" viewBox="0 0 24 21" width="24" xmlns="http://www.w3.org/2000/svg"><path d="m13.2045 20.9999-1.75-1.7273 7.2046-7.2046h-18.6591v-2.49995h18.6591l-7.2046-7.18182 1.75-1.75 10.1819 10.18177z" /></svg></i>`);
 
   return (
-    <ul className="w-full grid md:grid-cols-4 lg:grid-cols-12 gap-4 md:gap-12 2xl:gap-16 items-center">
-      <li className="md:col-span-2 lg:col-span-6 relative aspect-square h-screen w-full">
+    <ul className="w-full grid lg:grid-cols-4 lg:grid-cols-12 gap-4 lg:gap-12 2xl:gap-16 items-center">
+      <li className="lg:col-span-2 lg:col-span-6 relative aspect-square lg:h-screen w-full">
         <Image
           src={image}
           alt={`${imageTitle} – ${imageCredit}`}
@@ -34,17 +36,18 @@ export default function Step({
         />
         <p className="absolute left-4 bottom-4"><StandardText color="text-off-white"><strong>{imageTitle}</strong> &mdash; {imageCredit}</StandardText></p>
       </li>
-      <li className="md:col-span-2 lg:col-span-5 grid gap-2 md:gap-4">
+      <li className="lg:col-span-2 lg:col-span-5 grid gap-2 lg:gap-4 px-4 lg:px-0">
         {index === 0 &&
           <h2><MediumText color="text-black"><strong>Workflow</strong></MediumText></h2>
         }
-        <h3><MediumText color="text-black"><span className="underline decoration-1 underline-offset-4">Step {index + 1}:</span>{" "}{title}</MediumText></h3>
-        <p><MediumText color="text-black">{description}</MediumText></p>
-        <Link
-          href="#contact"
+        <h3 className="line-clamp-1 lg:line-clamp-none"><MediumText color="text-black"><span className="underline decoration-1 underline-offset-4">Step {index + 1}:</span>{" "}{title}</MediumText></h3>
+        <p className="line-clamp-4 lg:line-clamp-none"><MediumText color="text-black">{description}</MediumText></p>
+        <button
+          onClick={ctaAction}
+          className="text-left"
         >
           <MediumText color="text-black"><span dangerouslySetInnerHTML={{ __html: formattedLinkText }} /></MediumText>
-        </Link>
+        </button>
       </li>
     </ul>
   )
--- src/pages/architects-and-developers/workflow.tsx
+++ src/pages/architects-and-developers/workflow.tsx
@@ -3,9 +3,11 @@ import ReactFullpage from '@fullpage/react-fullpage';
 import CustomHead from '@/components/CustomHead';
 import Header from "@/components/Header";
 import Step from "@/components/workflow/Step";
-import Footer from "@/components/Footer";
+import Contact from '@/components/Contact';
+// import Footer from "@/components/Footer";
 
 import { attributes } from '@/content/architects-and-developers/workflow.md';
+import { attributes as globals } from '@/content/globals.md';
 
 const pluginWrapper = () => {
   require('@/utils/fullpage.scrollHorizontally.min.js');
@@ -13,6 +15,7 @@ const pluginWrapper = () => {
 
 export default function Workflow() {
   const { title, steps } = attributes;
+  const { contactTitle, contactText } = globals;
 
   interface Step {
     image: string;
@@ -32,33 +35,48 @@ export default function Workflow() {
         type="architects-and-developers"
       />
       <ReactFullpage
+        pluginWrapper={pluginWrapper}
+        controlArrows={false}
         credits={{ enabled: false }}
         licenseKey={process.env.NEXT_PUBLIC_FULLPAGE_LICENSE || ''}
-        pluginWrapper={pluginWrapper}
         scrollHorizontally={true}
+        verticalCentered={false}
         scrollHorizontallyKey={process.env.NEXT_PUBLIC_FULLPAGE_EXTENSION_ACTIVATION_KEY || ''}
-        render={() => (
+        render={({ fullpageApi }) => (
           <ReactFullpage.Wrapper>
-            {steps.map((step: Step, index: number) =>
-              <section
-                className="section"
-                key={index}
-              >
-                <Step
-                  index={index}
-                  image={step.image}
-                  imageTitle={step.imageTitle}
-                  imageCredit={step.imageCredit}
-                  title={step.title}
-                  description={step.description}
-                  cta={step.cta}
-                />
-              </section>
-            )}
+            <section
+              className="section"
+            >
+              {steps.map((step: Step, index: number) =>
+                <div
+                  className="slide h-screen"
+                  key={index}
+                >
+                  <Step
+                    index={index}
+                    image={step.image}
+                    imageTitle={step.imageTitle}
+                    imageCredit={step.imageCredit}
+                    title={step.title}
+                    description={step.description}
+                    ctaText={step.cta}
+                    ctaAction={() => fullpageApi.moveSectionDown()}
+                  />
+                </div>
+              )}
+            </section>
+            <section
+              className="section"
+            >
+              <Contact
+                title={contactTitle}
+                content={contactText}
+              />
+            </section>
           </ReactFullpage.Wrapper>
         )}
       />
-      <Footer classes="pb-4" />
+      {/* <Footer classes="pb-4" /> */}
     </main>
   )
 }
