4913ae8b78715a4310f3db88e7147c70d023210b
Juan Martín García <hello@juangarcia.com.ar>
2023-08-15 16:57:04 -0300

    Update meta description

--- src/components/CustomHead.tsx
+++ src/components/CustomHead.tsx
@@ -8,6 +8,7 @@ export default function CustomHead({
   return (
     <Head>
       <title>{`${title} • SSV`}</title>
+      <meta name="description" content="Sharp Studio Visuals. Digital rendering rervices company based in Aventura, FL. Ultra high quality images for Architects & Developers as well as a suite of solutions for Brokers & Realtors." />
     </Head>
   )
 }
