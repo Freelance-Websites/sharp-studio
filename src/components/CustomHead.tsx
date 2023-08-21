49aa3b8c9e1f79e3020b2a1fae728f1c8c124e6f
Juan Martín García <hello@juangarcia.com.ar>
2023-08-21 14:51:26 -0300

    Add decap CMS script

--- src/components/CustomHead.tsx
+++ src/components/CustomHead.tsx
@@ -1,3 +1,4 @@
+import Script from 'next/script';
 import Head from 'next/head';
 
 export default function CustomHead({
@@ -10,6 +11,8 @@ export default function CustomHead({
       <title>{`${title} • SSV`}</title>
       <meta name="description" content="Sharp Studio Visuals. Digital rendering rervices company based in Aventura, FL. Ultra high quality images for Architects & Developers as well as a suite of solutions for Brokers & Realtors." />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+      <Script src="https://identity.netlify.com/v1/netlify-identity-widget.js" />
     </Head>
   )
 }
