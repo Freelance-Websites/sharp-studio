e1a7e8a77ce86fb9a81f6a5271053dd1be4173fe
Juan Martín García <hello@juangarcia.com.ar>
2023-08-22 15:07:34 -0300

    Update meta tag

--- src/components/CustomHead.tsx
+++ src/components/CustomHead.tsx
@@ -9,7 +9,7 @@ export default function CustomHead({
   return (
     <Head>
       <title>{`${title} • SSV`}</title>
-      <meta name="description" content="Sharp Studio Visuals. Digital rendering rervices company based in Aventura, FL. Ultra high quality images for Architects & Developers as well as a suite of solutions for Brokers & Realtors." />
+      <meta name="description" content="Sharp Studio Visuals. Digital rendering services company based in Aventura, FL. Ultra high quality images for Architects & Developers as well as a suite of solutions for Brokers & Realtors." />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
       <Script src="https://identity.netlify.com/v1/netlify-identity-widget.js" />
