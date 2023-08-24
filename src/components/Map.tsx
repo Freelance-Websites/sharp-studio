2236f5d2b4340dd84f19f5eaeebe26787f6068ab
Juan Martín García <hello@juangarcia.com.ar>
2023-08-24 08:54:14 -0300

    Update map

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
+import Image from 'next/image';
 import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api';
-
-import { SmallText } from '@/components/Typography';
 
 const styles = [{"featureType":"water","elementType":"geometry","stylers":[{"color":"#e9e9e9"},{"lightness":17}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":20}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#ffffff"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#ffffff"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":16}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":21}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#dedede"},{"lightness":21}]},{"elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#ffffff"},{"lightness":16}]},{"elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#333333"},{"lightness":40}]},{"elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#f2f2f2"},{"lightness":19}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#fefefe"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#fefefe"},{"lightness":17},{"weight":1.2}]}];
 
@@ -28,24 +27,34 @@ const Map = () => {
   }, []);
 
   return isLoaded ? (
-    <GoogleMap
-      mapContainerStyle={containerStyle}
-      center={center}
-      zoom={17}
-      onUnmount={onUnmount}
-      options={{styles}}
-    >
-      <Marker
-        label={{
-          text: "20900 NE 30th Ave, Aventura, FL 33180, USA",
-          className: "shadow-lg bg-white p-2 rounded-md text-black text-sm tracking-wider font-neue-regular absolute -top-12 -left-40",
-        }}
-        position={center}
-        icon={{
-          url: '/images/icons/marker.svg',
-        }}
-      />
-    </GoogleMap>
+    <div className="grid grid-cols-1 lg:grid-cols-2">
+      <div className="relative w-full h-full border-solid border-r-white border-r-2 hidden lg:block">
+        <Image
+          src="/images/florida.jpg"
+          alt="Florida, USA"
+          fill={true}
+          className="w-full h-full object-cover"
+        />
+      </div>
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={17}
+        onUnmount={onUnmount}
+        options={{styles}}
+      >
+        <Marker
+          label={{
+            text: "20900 NE 30th Ave, Aventura, FL 33180, USA",
+            className: "shadow-lg bg-white p-2 rounded-md text-black text-sm tracking-wider font-neue-regular absolute -top-12 -left-40",
+          }}
+          position={center}
+          icon={{
+            url: '/images/icons/marker.svg',
+          }}
+        />
+      </GoogleMap>
+    </div>
   ) : <></>
 }
 
