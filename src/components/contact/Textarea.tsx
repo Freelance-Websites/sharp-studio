759d22deab03e58f771fb022972f5ecf65f93cad
Juan Martín García <hello@juangarcia.com.ar>
2023-08-25 16:37:37 -0300

    Add padding and name to inputs

--- src/components/contact/Textarea.tsx
+++ src/components/contact/Textarea.tsx
@@ -16,9 +16,10 @@ export default function Textarea({
       <label className="block mb-2" htmlFor={id}><SmallText color="text-black">{label}</SmallText></label>
       <textarea
         id={id}
+        name={id}
         placeholder={placeholder}
         required={required}
-        className="text-xl md:text-2xl xl:text-3xl xl:leading-normal placeholder:text-black/20 pb-3 border-b border-b-black/10 focus:outline-none text-black w-full resize-none md:h-16 bg-off-white"
+        className="text-xl md:text-2xl xl:text-3xl xl:leading-normal placeholder:text-black/20 pl-3 pb-3 border-b border-b-black/10 focus:outline-none text-black w-full resize-none md:h-16 bg-off-white"
       />
     </div>
   )
