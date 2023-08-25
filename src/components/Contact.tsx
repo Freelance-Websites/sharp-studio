03f6d6ff5461ade659619789bd9f2c18743b4ba1
Juan Martín García <hello@juangarcia.com.ar>
2023-08-25 15:48:54 -0300

    Add name to form

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -20,7 +20,7 @@ export default function Contact({
     <div className="container mx-auto px-4 grid gap-2">
       <h4><UpperCaseText color="text-black">{title}</UpperCaseText></h4>
       <p><MediumText color="text-black">{content}</MediumText></p>
-      <form netlify className="grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-8 mt-4 md:mt-8 max-w-4xl">
+      <form netlify name="contact" className="grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-8 mt-4 md:mt-8 max-w-4xl">
         <Input
           type="text"
           id="name"
