1d9093c19595035b15fa0c134f703aa417283c57
Juan Martín García <hello@juangarcia.com.ar>
2023-08-29 14:34:50 -0300

    WIP: Try a higher tocuhSensitivy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -37,6 +37,7 @@ export default function Home() {
       <ReactFullpage
         credits={{ enabled: false }}
         licenseKey={process.env.NEXT_PUBLIC_FULLPAGE_LICENSE || ''}
+        touchSensitivity={100}
         render={() => (
           <ReactFullpage.Wrapper>
             <section className="section">
