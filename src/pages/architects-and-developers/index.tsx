7bae64f6e5fa7ccf27004fbf96aa9fa8e1e91a51
Juan Martín García <hello@juangarcia.com.ar>
2023-08-29 14:55:21 -0300

    WIP: Try a larger sensitivity

--- src/pages/architects-and-developers/index.tsx
+++ src/pages/architects-and-developers/index.tsx
@@ -60,7 +60,7 @@ export default function Home() {
       <ReactFullpage
         credits={{ enabled: false }}
         licenseKey={process.env.NEXT_PUBLIC_FULLPAGE_LICENSE || ''}
-        touchSensitivity={15}
+        touchSensitivity={30}
         render={() => (
           <ReactFullpage.Wrapper>
             {heroEnabled &&
