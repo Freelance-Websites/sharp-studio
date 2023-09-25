e94e6775a55f04707820f2e89b4b4e16ba98e197
Juan Martín García <hello@juangarcia.com.ar>
2023-09-25 09:57:06 -0300

    Add virtual staging URL

---
+++ src/pages/brokers-and-realtors/services/virtual-staging.tsx
@@ -0,0 +1,13 @@
+import VirtualStaging from "@/components/sections/VirtualStaging"
+
+export default function servicesVirtualStaging() {
+  return (
+    <VirtualStaging
+      leftImage="/images/brokers-and-realtors/services/interactive-experiences-slider-5.jpg"
+      rightImage="/images/brokers-and-realtors/services/interactive-experiences-slider-6.jpg"
+      title="LIV Thames"
+      credit="ATV"
+      percentage={0.5}
+    />
+  )
+}
