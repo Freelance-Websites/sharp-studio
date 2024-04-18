a9191ed853f60f026bca9f804e8fea423fc56c9c
Juan Martin Garcia <hi@juanmartingarcia.com>
2024-04-18 13:45:07 -0300

    Make videos autoplay

--- src/components/portfolio/VimeoEmbed.tsx
+++ src/components/portfolio/VimeoEmbed.tsx
@@ -7,7 +7,7 @@ export default function VimeoEmbed({ url }: {
         className="relative aspect-video"
       >
         <iframe
-          src={`https://player.vimeo.com/video/${url?.split('vimeo.com/')[1]}?h=5764ab8c22&&title=0&byline=0&portrait=0&badge=0`}
+          src={`https://player.vimeo.com/video/${url?.split('vimeo.com/')[1]}?h=5764ab8c22&autoplay=1&loop=1&autopause=0&title=0&byline=0&portrait=0&badge=0`}
           className="absolute top-0 left-0 w-full h-full"
           frameBorder="0"
           allow="autoplay; fullscreen; picture-in-picture"
