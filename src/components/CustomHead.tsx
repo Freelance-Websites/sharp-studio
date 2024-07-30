93fb55701c75d5a543f07b5949c6e08377b9ab87
Juan Martin Garcia <hi@juanmartingarcia.com>
2024-07-30 12:18:01 -0300

    Add open graph image

--- src/components/CustomHead.tsx
+++ src/components/CustomHead.tsx
@@ -16,10 +16,20 @@ export default function CustomHead({
   return (
     <Head>
       <title>{`${title} • SSV`}</title>
-      <meta name="description" content="Sharp Studio Visuals. Digital rendering services company based in Aventura, FL. Ultra high quality images for Architects & Developers as well as a suite of solutions for Brokers & Realtors." />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-      <Script src="https://identity.netlify.com/v1/netlify-identity-widget.js" />
+      <meta name="description" content="Sharp Studio Visuals. Digital rendering services company based in Aventura, FL. Ultra high quality images for Architects & Developers as well as a suite of solutions for Brokers & Realtors." />
+      
+      <meta property="og:url" content="https://sharpstudiovisuals.com" />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="Sharp Studio Visuals" />
+      <meta property="og:description" content="Sharp Studio Visuals. Digital rendering services company based in Aventura, FL. Ultra high quality images for Architects & Developers as well as a suite of solutions for Brokers & Realtors." />
+      <meta property="og:image" content="https://sharpstudiovisuals.com/images/logo.png" />
+
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content="Sharp Studio Visuals" />
+      <meta name="twitter:description" content="Sharp Studio Visuals. Digital rendering services company based in Aventura, FL. Ultra high quality images for Architects & Developers as well as a suite of solutions for Brokers & Realtors." />
+      <meta name="twitter:image" content="https://sharpstudiovisuals.com/images/logo.png" />
     </Head>
   )
 }
